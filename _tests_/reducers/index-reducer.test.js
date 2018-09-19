55fd33f0d971a117d11d5a623134a8d6b732b181
Guest User <Guest@epicodus-41.local>
2018-09-19 14:27:09 -0700

    combining reducers

--- _tests_/reducers/index-reducer.test.js
+++ _tests_/reducers/index-reducer.test.js
@@ -1,4 +1,9 @@
 import rootReducer from './../../src/reducers/index';
+import selectedTicketReducer from './../../src/reducers/selected-ticket-reducer';
+import ticketListReducer from './../../src/reducers/ticket-list-reducer';
+import { createStore } from 'redux';
+
+let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
 
@@ -8,5 +13,11 @@ describe("rootReducer", () => {
       selectedTicket: {}
     });
   });
+  test('Should contain ticketListReducer logic', () => {
+    expect(store.getState().masterTicketList).toEqual(ticketListReducer(undefined, { type: null }));
+  });
 
+  test('Should contain selectedTicketReducer logic', () => {
+    expect(store.getState().selectedTicket).toEqual(selectedTicketReducer(undefined, { type: null }));
+  });
 });
